b21d7a9d00ace071b34764dd27afca0fb32d78a7
PEINTRE <aryayung@foxmail.com>
2022-04-19 16:16:39 +0800

    Add MediumQuestion - lexicalOrder

--- LeetCodePass/MediumQ.cpp
+++ LeetCodePass/MediumQ.cpp
@@ -162,15 +162,28 @@ NestedInteger MediumQ::deserialize(string s) {
  *	Question : 字典序排数
  *	给你一个整数 n ，按字典序返回范围 [1, n] 内所有整数。
  *	你必须设计一个时间复杂度为 O(n) 且使用 O(1) 额外空间的算法。
-
+ *
  *	Date	 : [ 2022/04/18 16:28:50 ]
  */
+//@NOPASS
 vector<int> MediumQ::lexicalOrder(int n) {
-	vector<int> ans;
+	vector<int> ans(n, 0);
 	int num = 1;
-	while (num <= n)
+	for (int i = 0; i < n; i++)
 	{
-
+		ans[i] = num;
+		if (num*10 <= n)
+		{
+			num *= 10;
+		}
+		else
+		{
+			while (num % 10 == 9 || num + 1 > n)
+			{
+				num /= 10;
+			}
+			num++;
+		}
 	}
 	return ans;
 }
